2130a6b461d90166c9e79ede8f74a24e1f9b7304
Guy de Messimy <gdemessimy@gmail.com>
2021-01-29 22:34:12 +0100

    write CSS naming

--- src/components/Entreprise/index.jsx
+++ src/components/Entreprise/index.jsx
@@ -1,30 +1,58 @@
-const Agribray = () => {
-    return (
-        <div className="Container">
-            {/* Colonne 1 */}
-            <div className="col1">
-                <h2>Bienvenue chez Agribray</h2>
-                <p>Lorepsum idemnsjzdib siakakb hsazhbs</p>
-                <button></button>
-            </div>      
-            {/* Colonne 2 */}
-            <div className="col2">
-                {/* Colonne a */}
-                <div className="travaux agricoles">
-                    <img src="" alt="" />
-                    <h3>Travaux agricoles</h3>
-                    <p>Lorepsum sbdisqdqihjdqisd</p>
-                </div>
-                {/* Colonne b */}
-                <div className="transports">
-                    <img src="" alt="" />
-                    <h3>Transports</h3>
-                    <p>Lorepsum ncsjhcis scsccsc</p>
-                </div>
-            </div>
-        </div>
-       
-    )
-}
+import {
+  Container,
+  Colonne1,
+  AgriTitle,
+  Paragraph1,
+  Colonne2,
+  TravauxAgricoles,
+  Image1,
+  TravauxTitle,
+  Paragraph2,
+  Transports,
+  Image2,
+  TransportTitle,
+  Paragraph3,
+} from "./style";
 
-export default Agribray
+const Agribray = () => {
+  return (
+    <Container>
+      <Colonne1>
+        <AgriTitle>Bienvenue chez Agribray</AgriTitle>
+        <Paragraph1>
+          La qualité des travaux est primordial pour vos cultures. C'est
+          pourquoi depuis plus de 34 ans nous mettons nos machines,
+          savoirs-faire et expertise à votre service. Nos prestations sont
+          adaptées à vos problématiques saisonnières : semis, épandage, lutte
+          contre le gel, maladies, moissons ... rapidité d'interventio,
+          conseils, parc matériel, personnel qualifié, experience ...
+        </Paragraph1>
+        <button>Test</button>
+      </Colonne1>
+      <Colonne2>
+        <TravauxAgricoles>
+          <Image1 src="./photos/Tracteur.jpg" width="150px" alt="tracteur" />
+          <TravauxTitle>Travaux agricoles</TravauxTitle>
+          <Paragraph2>
+            Travaux du sol, semences, semis, épandage, application de produit
+            phytosanitaire, récolte de céréales, fourrages, betteraves,
+            évacuation des récoltes, gestion agricole (cartographie du sol),
+            ensilage, battage, déchaumage, moisson ...
+          </Paragraph2>
+        </TravauxAgricoles>
+        <Transports>
+          <Image2 src="./photos/Camion.jpg" width="150px" alt="camion" />
+          <TransportTitle>Transports</TransportTitle>
+          <Paragraph3>
+            Transports de marchandise en vrac : céréales, pommes de terre,
+            légumes, engrais, matériaux ... préciser les matériels (tapis,
+            benne, fond mouvant, ampliroll, tracto benne), agrément, zone de
+            travail, ports, qualification, chauffeur ...
+          </Paragraph3>
+        </Transports>
+      </Colonne2>
+    </Container>
+  );
+};
+
+export default Agribray;
---
+++ src/components/Entreprise/style.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: solid 1px red;
+  display: flex;
+  flex-direction: columnn;
+`;
+export const Colonne1 = styled.div`
+  border: solid 1px blue;
+`;
+
+export const AgriTitle = styled.h1``;
+export const Paragraph1 = styled.p``;
+export const Colonne2 = styled.div`
+  border: solid 1px green;
+  display: flex;
+`;
+
+export const TravauxAgricoles = styled.div``;
+export const Image1 = styled.img``;
+export const TravauxTitle = styled.h2``;
+export const Paragraph2 = styled.p``;
+export const Transports = styled.div``;
+export const Image2 = styled.img``;
+export const TransportTitle = styled.h2``;
+export const Paragraph3 = styled.p``;
