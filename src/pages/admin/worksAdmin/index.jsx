2aeeb0eb1b0893cf514fe9685a39c1980e39a9c7
Guy de Messimy <gdemessimy@gmail.com>
2021-02-04 20:30:11 +0100

    create admin works components

--- src/pages/admin/worksAdmin/index.jsx
+++ src/pages/admin/worksAdmin/index.jsx
@@ -3,12 +3,47 @@ import { useState } from "react";
 import Works from "../../../components/works";
 import { useSelector, useDispatch } from "react-redux";
 import { addWorksAction } from "../../../redux/actions";
+import axios from "axios";
 
 const TravauxList = () => {
+
   const [work, setWork] = useState([]);
   const works = useSelector((state) => state.works.data);
   const dispatch = useDispatch();
   console.log(works);
+
+  const [form, setForm] = useState({
+    prenom: "",
+    nom: "",
+    mail: "",
+    tel: "",
+    societe: "",
+    cp: "",
+    ville: "",
+    prestation: "",
+    commentaire: "",
+  });
+
+  const handleChange = (e) => {
+    console.log("got field " + e.target.name + ", value " + e.target.value);
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const { nom, mail, prestation } = form;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5050/contact", form)
+      .then(({ data }) => {
+        console.log("Comic was created");
+      })
+      .catch((err) => {
+        console.warn("Something went poorly");
+      });
+    console.log(form);
+  };
+
 
   const workOnChange = (event) => {
     setWork(event.target.value);
@@ -25,11 +60,21 @@ const TravauxList = () => {
       <button type="button" onClick={addWork}>
         Add Task
       </button>
-      <div>
-        {work.map(({ id, nature, image1 }) => {
-          return <Works key={id} id={id} nature={nature} image1={image1} />;
-        })}
-      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Prenom:
+          <input
+            name="prenom"
+            type="text"
+            placeholder="Didier"
+            onChange={handleChange}
+            value={form.prenom}
+            required
+          />
+        </label>
+        <br />
+        <input type="Submit" readOnly value="Nous contacter!" />
+      </form>
     </>
   );
 };
