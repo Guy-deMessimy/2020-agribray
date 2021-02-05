b065620b9cf64d946e57e77f61d9e0c531049ab3
Guy de Messimy <gdemessimy@gmail.com>
2021-02-05 09:39:38 +0100

    test redux

--- src/redux/reducers/works.jsx
+++ src/redux/reducers/works.jsx
@@ -1,12 +1,12 @@
 const InitialState = {
   nature: "",
+  image1: "",
 };
 
 const works = (state = InitialState, action) => {
   switch (action.type) {
     case "ADD_WORKS":
-      return {...state, nature: action.title
-      };
+      return { ...state, nature: action.title };
     case "REMOVE_WORKS":
       return {
         ...state,
